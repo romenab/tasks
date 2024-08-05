a9a5ee385dc5e5bc9871c607ab45e38b865cedfa
Romena Beķere <162146249+romenab@users.noreply.github.com>
2024-08-05 14:57:37 +0300

    Update arith.php

--- arith.php
+++ arith.php
@@ -1,72 +1,77 @@
 <?php
-function addition($case)
+function addition(string $case): string
 {
+    $output = "";
     $expression = explode("+", $case);
     $first = $expression[0];
     $second = $expression[1];
     $lengthFirst = strlen($first);
     $lengthSecond = strlen($second);
     if ($lengthFirst < $lengthSecond + 1) {
-        echo str_repeat(" ", $lengthSecond + 1 - $lengthFirst);
+        $output .= str_repeat(" ", $lengthSecond + 1 - $lengthFirst);
     }
-    echo $first . PHP_EOL;
+    $output .= $first . PHP_EOL;
     if ($lengthFirst > $lengthSecond + 1) {
-        echo str_repeat(" ", $lengthFirst - $lengthSecond - 1);
+        $output .= str_repeat(" ", $lengthFirst - $lengthSecond - 1);
     }
-    echo "+" . $second . PHP_EOL;
+    $output .= "+" . $second . PHP_EOL;
     if ($lengthFirst < $lengthSecond + 1) {
-        echo str_repeat("-", $lengthSecond + 1);
+        $output .= str_repeat("-", $lengthSecond + 1);
     } else {
-        echo str_repeat("-", $lengthFirst);
+        $output .= str_repeat("-", $lengthFirst);
     }
-    echo PHP_EOL;
+    $output .= PHP_EOL;
 
     $sum = (int)$first + (int)$second;
     $lengthSum = strlen((string)$sum);
 
     for ($j = $lengthSum; $j < $lengthSecond + 1; $j++) {
-        echo " ";
+        $output .= " ";
     }
 
-    echo $sum . PHP_EOL;
-    echo PHP_EOL;
+    $output .= $sum . PHP_EOL;
+    $output .= PHP_EOL;
+    return $output;
 }
 
-function subtraction($case)
+function subtraction(string $case): string
 {
+    $output = "";
     $expression = explode("-", $case);
     $first = $expression[0];
     $second = $expression[1];
     $lengthFirst = strlen($first);
     $lengthSecond = strlen($second);
     if ($lengthFirst < $lengthSecond + 1) {
-        echo str_repeat(" ", $lengthSecond + 1 - $lengthFirst);
+        $output .= str_repeat(" ", $lengthSecond + 1 - $lengthFirst);
     }
-    echo $first . PHP_EOL;
+    $output .= $first . PHP_EOL;
     if ($lengthFirst > $lengthSecond + 1) {
-        echo str_repeat(" ", $lengthFirst - $lengthSecond - 1);
+        $output .= str_repeat(" ", $lengthFirst - $lengthSecond - 1);
     }
-    echo "-" . $second . PHP_EOL;
+    $output .= "-" . $second . PHP_EOL;
     if ($lengthFirst < $lengthSecond + 1) {
-        echo str_repeat("-", $lengthSecond + 1);
+        $output .= str_repeat("-", $lengthSecond + 1);
     } else {
-        echo str_repeat("-", $lengthFirst);
+        $output .= str_repeat("-", $lengthFirst);
     }
-    echo PHP_EOL;
+    $output .= PHP_EOL;
 
     $difference = (int)$first - (int)$second;
     $length = strlen((string)$difference);
 
     for ($j = $length; $j < $lengthSecond + 1; $j++) {
-        echo " ";
+        $output .= " ";
     }
 
-    echo $difference . PHP_EOL;
-    echo PHP_EOL;
+    $output .= $difference . PHP_EOL;
+    $output .= PHP_EOL;
+    return $output;
 }
 
-function multiplication($case)
+function multiplication(string $case): string
 {
+    $output = "";
     $expression = explode("*", $case);
     $first = $expression[0];
     $second = $expression[1];
@@ -78,55 +83,58 @@ function multiplication($case)
     $spaces = $lengthResult - $lengthSecond - 1;
 
     if ($lengthSecond + 1 > $lengthFirst) {
-        echo str_repeat(" ", $spaces);
+        $output .= str_repeat(" ", $spaces);
         for ($i = $lengthFirst; $i < $lengthSecond + 1; $i++) {
-            echo " ";
+            $output .= " ";
         }
-        echo $first . PHP_EOL;
-        echo str_repeat(" ", $spaces);
-        echo "*" . $second . PHP_EOL;
-        echo str_repeat(" ", $spaces);
-        echo str_repeat("-", $lengthSecond + 1);
+        $output .= $first . PHP_EOL;
+        $output .= str_repeat(" ", $spaces);
+        $output .= "*" . $second . PHP_EOL;
+        $output .= str_repeat(" ", $spaces);
+        $output .= str_repeat("-", $lengthSecond + 1);
     }
 
     if ($lengthFirst > $lengthSecond + 1) {
-        echo $first . PHP_EOL;
+        $output .= $first . PHP_EOL;
         for ($i = $lengthSecond + 1; $i < $lengthFirst; $i++) {
-            echo " ";
+            $output .= " ";
         }
-        echo "*" . $second . PHP_EOL;
-        echo str_repeat("-", $lengthFirst);
+        $output .= "*" . $second . PHP_EOL;
+        $output .= str_repeat("-", $lengthFirst);
     }
-    echo PHP_EOL;
+    $output .= PHP_EOL;
 
     $count = 0;
     $digitSecond = str_split($second);
 
     for ($i = $lengthSecond - 1; $i >= 0; $i--) {
         $multiply = (int)$digitSecond[$i] * (int)$first;
-        echo str_repeat(" ", $i);
-        echo $multiply . PHP_EOL;
+        $output .= str_repeat(" ", $i);
+        $output .= $multiply . PHP_EOL;
         $count++;
     }
     if ($count > 1) {
-        echo str_repeat("-", $lengthResult);
-        echo PHP_EOL;
-        echo $result . PHP_EOL;
+        $output .= str_repeat("-", $lengthResult);
+        $output .= PHP_EOL;
+        $output .= $result . PHP_EOL;
     }
-    echo PHP_EOL;
+    $output .= PHP_EOL;
+    return $output;
 }
 
-$testCases = 4;
-$cases = ["12345+67890", "324-111", "325*4405", "1234*4"];
-
+echo "Enter the number of test cases: ";
+$testCases = (int)trim(fgets(STDIN));
+$cases = [];
 for ($i = 0; $i < $testCases; $i++) {
-    if (strpos($cases[$i], "+")) {
-        addition($cases[$i]);
-    }
-    if (strpos($cases[$i], "-")) {
-        subtraction($cases[$i]);
-    }
-    if (strpos($cases[$i], "*")) {
-        multiplication($cases[$i]);
+    echo "Enter test case " . ($i + 1) . ": ";
+    $cases[] = trim(fgets(STDIN));
+}
+foreach ($cases as $case) {
+    if (strpos($case, "+")) {
+        echo addition($case);
+    } elseif (strpos($case, "-")) {
+        echo subtraction($case);
+    } elseif (strpos($case, "*")) {
+        echo multiplication($case);
     }
 }
